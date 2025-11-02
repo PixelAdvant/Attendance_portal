7e2b1b23a26049fcb6aa0adafb03939fb2da7050
Imran0531 <shaikimran97108@gmail.com>
2025-11-02 11:15:41 +0530

    Fix: switch to HashRouter for GitHub Pages support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
+// import ManagerLayout from "./components/ManagerLayout";
+// import { getLoggedInManager } from "./utils/storage";
+
+// function App() {
+//   const manager = getLoggedInManager();
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route
+//           path="/portal/*"
+//           element={manager ? <ManagerLayout /> : <Navigate to="/" />}
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ManagerLayout from "./components/ManagerLayout";
